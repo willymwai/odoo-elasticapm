d5abe6ed091e5dbe8ba09a46b9f345b6c532c470
William Mwai <willymwaid92@gmail.com>
2022-08-24 15:27:48 +0300

    chore: instrument fields

---
+++ odoo_elasticapm/fields.py
@@ -0,0 +1,20 @@
+from odoo_elasticapm.base import build_params, capture_exception, elasticapm
+
+try:
+    from odoo.fields import Field, Id, One2Many
+except ImportError:
+    from openerp.fields import Field, Id, One2Many
+
+
+for field in [Field, Id, One2Many]:
+    ori_get = field.__get__
+
+    def __get__(self, obj, owner):
+        with elasticapm.capture_span(**build_params(self, "__get__")):
+            try:
+                return ori_get(self, obj, owner)
+            except Exception as e:
+                capture_exception(e)
+                raise
+
+    field.__get__ = __get__

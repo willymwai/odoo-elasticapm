328ee9260d2614d5c28b547bfaec9d305b87b95d
William Mwai <willymwaid92@gmail.com>
2022-08-24 16:06:27 +0300

    chore: disable fields instrumentation

--- odoo_elasticapm/__init__.py
+++ odoo_elasticapm/__init__.py
@@ -5,4 +5,4 @@ from . import ir_http  # noqa
 from . import models  # noqa
 from . import ir_cron  # noqa
 from . import res_partner  # noqa
-from . import fields  # noqa
+# from . import fields  # noqa

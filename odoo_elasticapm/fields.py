17e95c3e76554d584ba79ca9b9405a3c5dd7a4c6
William Mwai <willymwaid92@gmail.com>
2022-08-25 08:06:58 +0300

    chore: instrument fields

--- odoo_elasticapm/fields.py
+++ odoo_elasticapm/fields.py
@@ -5,17 +5,38 @@ try:
 except ImportError:
     from openerp.fields import Field, Id, One2many
 
+ori_field_get = Field.__get__
+ori_id_get = Id.__get__
+ori_one2many_get = One2many.__get__
 
-for field in [Field, Id, One2many]:
-    ori_get = field.__get__
 
-    def __get__(self, obj, owner):
-        if isinstance(self, field):
-            with elasticapm.capture_span(**build_params(self, "__get__")):
-                try:
-                    return ori_get(self, obj, owner)
-                except Exception as e:
-                    capture_exception(e)
-                    raise
+def field_get(self, obj, owner):
+    with elasticapm.capture_span(**build_params(self, "__get__")):
+        try:
+            return ori_field_get(self, obj, owner)
+        except Exception as e:
+            capture_exception(e)
+            raise
 
-    field.__get__ = __get__
+
+def id_get(self, obj, owner):
+    with elasticapm.capture_span(**build_params(self, "__get__")):
+        try:
+            return ori_id_get(self, obj, owner)
+        except Exception as e:
+            capture_exception(e)
+            raise
+
+
+def one2many_get(self, obj, owner):
+    with elasticapm.capture_span(**build_params(self, "__get__")):
+        try:
+            return ori_one2many_get(self, obj, owner)
+        except Exception as e:
+            capture_exception(e)
+            raise
+
+
+Field.__get__ = field_get
+Id.__get__ = id_get
+One2many.__get__ = one2many_get

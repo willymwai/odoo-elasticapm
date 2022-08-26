5926f162e0fe192617f711a7d86f17b6334cf6af
William Mwai <willymwaid92@gmail.com>
2022-08-26 11:00:46 +0300

    debug: get created span

--- odoo_elasticapm/fields.py
+++ odoo_elasticapm/fields.py
@@ -20,7 +20,8 @@ def field_get(self, obj, owner):
 
 
 def id_get(self, obj, owner):
-    with elasticapm.capture_span(**build_params(self, "__get__")):
+    with elasticapm.capture_span(**build_params(self, "__get__")) as span:
+        print("span", span)
         try:
             return ori_id_get(self, obj, owner)
         except Exception as e:

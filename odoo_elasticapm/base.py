c833491c139b5d5d2996698da54fd262b572c88b
William Mwai <willymwaid92@gmail.com>
2022-08-26 15:12:54 +0300

    chore: pass full exception stacktrace

--- odoo_elasticapm/base.py
+++ odoo_elasticapm/base.py
@@ -2,6 +2,7 @@
 
 
 import os
+import sys
 import traceback
 
 try:
@@ -76,7 +77,7 @@ def get_data_from_request():
 
 def capture_exception(exception, is_http_request=False):
     handled = False
-    message = traceback.format_exc()
+    exc_info = sys.exc_info()
     for exception_class in EXCEPTIONS:
         if isinstance(exception, exception_class):
             handled = True
@@ -89,10 +90,10 @@ def capture_exception(exception, is_http_request=False):
         elastic_apm_client.capture_exception(
             context={"request": get_data_from_request()},
             handled=handled,
-            exc_info=message,
+            exc_info=exc_info,
         )
     else:
-        elastic_apm_client.capture_exception(handled=handled, exc_info=message)
+        elastic_apm_client.capture_exception(handled=handled, exc_info=exc_info)
 
 
 def build_params(self, method):

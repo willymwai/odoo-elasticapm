71307804d6a81abfda69f3e11b5e05110b4d58a2
William Mwai <willymwaid92@gmail.com>
2022-08-27 09:06:25 +0300

    chore: instrument python threading

--- odoo_elasticapm/threading.py
+++ odoo_elasticapm/threading.py
@@ -4,16 +4,16 @@ import elasticapm
 
 from odoo_elasticapm.base import build_params, capture_exception
 
-ori_run = threading.Thread.run
+ori_bootstrap_inner = threading.Thread._bootstrap_inner
 
 
-def run(self):
+def _bootstrap_inner(self):
     try:
-        with elasticapm.capture_span(**build_params(self, "run")):
-            return ori_run(self)
+        with elasticapm.capture_span(**build_params(self, "_bootstrap_inner")):
+            return ori_bootstrap_inner(self)
     except Exception as e:
         capture_exception(e)
         raise
 
 
-threading.Thread.run = run
+threading.Thread._bootstrap_inner = _bootstrap_inner

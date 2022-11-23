f21915b0cc4950faf4e4348fcd8f52cd01fb4470
William Mwai <willymwaid92@gmail.com>
2022-11-23 10:23:48 +0300

    chore: use setup_scm==0.0.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     use_scm_version=True,
     packages=["odoo_elasticapm"],
     include_package_data=True,
-    setup_requires=["setuptools_scm"],
+    setup_requires=["setuptools_scm==0.0.1"],
     install_requires=["elastic-apm>=5.4"],
     license="AGPLv3+",
     author="Akretion",
